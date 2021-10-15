7ab481a6b1e135e680423750083a22cdc03fd00c
Marcus <marcusmiguel0612@gmail.com>
2021-10-15 11:43:38 -0300

    Removed Repository from Controller

--- WithPattern/WithPattern.WebApi/Controllers/PersonController.cs
+++ WithPattern/WithPattern.WebApi/Controllers/PersonController.cs
@@ -1,73 +1,70 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using WithPattern.Application.Repositories;
 using WithPattern.Application.UseCases;
 using WithPattern.Domain.Entities;
 
 namespace WithPattern.WebApi.Controllers
 {
-  [ApiController]
-  [Route("people")]
-  public class PersonController : ControllerBase
-  {
-    private readonly IMediator _mediator;
-    private readonly IPersonRepository _repository;
-
-    public PersonController(IMediator mediator, IPersonRepository repository)
+ 
+    [Route("people"
+    public class Pers
+    public class PersonController : ControllerBase
     {
-      _mediator = mediator;
-      _repository = repository;
-    }
+        private readonly IMediator _mediator;
 
-    [HttpGet("")]
-    public async Task<IEnumerable<Person>> GetAsync()
-    {
-      return await _mediator.Send(new GetPeople());
-    }
+        {
+        {
+          }
+        }
 
-    [HttpGet("{id:guid}")]
-    public async Task<IActionResult> GetByIdAsync(Guid id)
-    {
-      var p = await _mediator.Send(new GetPerson(id));
-      if (p != null)
-      {
-        return Ok(p);
-      }
-      return NotFound();
-    }
+        [HttpGet("")]
+        public async Task<IEnumerable<Person>> GetAsync()
+        {
+            return await _mediator.Send(new GetPeople());
+        }
 
-    [HttpPost("")]
-    public async Task<IActionResult> PostAsync(CreatePerson person)
-    {
-      var p = await _mediator.Send(person);
-      if (p != null)
-      {
-        return Ok(p);
-      }
-      return BadRequest();
-    }
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetByIdAsync(Guid id)
+        {
+            var p = await _mediator.Send(new GetPerson(id));
+            if (p != null)
+            {
+                return Ok(p);
+            }
+            return NotFound();
+        }
 
-    [HttpPut("{id:guid}")]
-    public async Task<IActionResult> PutAsync([FromRoute] Guid id, [FromBody] UpdatePerson person)
-    {
-      person.Id = id;
-      var p = await _mediator.Send(person);
-      if (p != null)
-      {
-        return Ok(p);
-      }
-      return NotFound();
-    }
+        [HttpPost("")]
+        public async Task<IActionResult> PostAsync(CreatePerson person)
+        {
+            var p = await _mediator.Send(person);
+            if (p != null)
+            {
+                return Ok(p);
+            }
+            return BadRequest();
+        }
 
-    [HttpDelete("{id:guid}")]
-    public async Task<IActionResult> DeleteAsync(Guid id)
-    {
-      var p = await _mediator.Send(new DeletePerson(id));
-      if (p != null)
-      {
-        return Ok(p);
-      }
-      return NotFound();
-    }
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> PutAsync([FromRoute] Guid id, [FromBody] UpdatePerson person)
+        {
+            person.Id = id;
+            var p = await _mediator.Send(person);
+            if (p != null)
+            {
+                return Ok(p);
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteAsync(Guid id)
+        {
+            var p = await _mediator.Send(new DeletePerson(id));
+            if (p != null)
+            {
+                return Ok(p);
+            }
+            return NotFound();
+        }
   }
-}
